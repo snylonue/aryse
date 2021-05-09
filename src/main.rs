5fd87fff4f8b1ab454ae4be4ab889aaf64629352
snylonue <snylonue@gmail.com>
2021-05-09 10:15:52 +0800

    dep: update bevy to 0.5

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,76 @@
 use bevy::prelude::*;
-use std::path::{Path, PathBuf};
+use bevy::input::keyboard::KeyboardInput;
+use std::path::Path;
+use std::path::PathBuf;
+
+mod cli;
 
 #[derive(Debug, Clone, Default)]
 struct Playlist {
     pub sources: Vec<PathBuf>,
     pub pos: usize,
 }
-
 struct OpenImage(PathBuf);
 
 struct ImageTimer(Timer);
 
 impl Playlist {
+    pub fn new(sources: Vec<PathBuf>) -> Self {
+        Self { sources, pos: 0 }
+    }
     pub fn next(&mut self) -> &Path {
+        let tmp = self.sources[self.pos].as_path();
         self.pos = (self.pos + 1) % self.sources.len();
-        self.sources[self.pos].as_path()
+        tmp
+    }
+}
+
+fn setup(mut commands: Commands) {
+    let app = cli::app().get_matches();
+    let images = app.values_of("image").unwrap().map(Into::into).collect();
+    let playlist = Playlist::new(images);
+    commands.insert_resource(playlist);
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn open_image(
+    mut ev_open_img: EventReader<OpenImage>,
+    asset_sever: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut commands: Commands,
+) {
+    for OpenImage(ref path) in ev_open_img.iter() {
+        let handle = asset_sever.load(dbg!(path).as_path());
+        commands.spawn_bundle(SpriteBundle {
+            material: materials.add(handle.into()),
+            ..Default::default()
+        });
+    }
+}
+
+fn keyboard(
+    mut ev_key: EventReader<KeyboardInput>,
+    mut ev_open_img: EventWriter<OpenImage>,
+    mut playlist: ResMut<Playlist>,
+) {
+    for ev in ev_key.iter() {
+        match ev.state {
+            bevy::input::ElementState::Pressed if ev.key_code == Some(KeyCode::Right) => {
+                let img = playlist.next();
+                ev_open_img.send(OpenImage(img.to_owned()));
+            }
+            _ => {}
+        }
     }
 }
 
 fn main() {
     App::build()
-        .add_plugins(DefaultPlugins)
         .add_event::<OpenImage>()
-        .add_resource(ImageTimer(Timer::from_seconds(1.0, true)))
-        // .add_resource(Playlist {
-            // sources: vec![
-                // PathBuf::from(r"D:\Files\Image\collection\pixiv\87165096_p0.png"),
-                // PathBuf::from(r"D:\Files\Image\collection\pixiv\73739752_p0.png")
-            // ],
-            // pos: 0
-        // })
-        .init_resource::<Playlist>()
-        .add_startup_system(add_image.system())
-        .add_system(show_image.system())
+        .insert_resource(ImageTimer(Timer::from_seconds(1.0, true)))
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
         .add_system(open_image.system())
+        .add_system(keyboard.system())
         .run();
-}
-
-fn add_image(commands: &mut Commands,mut playlist: ResMut<Playlist>) {
-    commands.spawn(Camera2dBundle::default());
-    playlist.sources.extend(vec![
-        PathBuf::from(r"D:\Files\Image\collection\pixiv\87165096_p0.png"),
-        PathBuf::from(r"D:\Files\Image\collection\pixiv\73739752_p0.png")
-    ]);
-}
-fn show_image(
-    mut ev_open_img: ResMut<Events<OpenImage>>,
-    mut playlist: ResMut<Playlist>,
-    time: Res<Time>,
-    mut timer: ResMut<ImageTimer>,
-) {
-    if !timer.0.tick(time.delta_seconds()).just_finished() || playlist.sources.len() == 0 {
-        return;
-    }
-    ev_open_img.send(OpenImage(playlist.next().into()));
-}
-fn open_image(
-    events: Res<Events<OpenImage>>,
-    mut ev_reader: Local<EventReader<OpenImage>>,
-    asset_sever: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    commands: &mut Commands,
-) {
-    for OpenImage(ref path) in ev_reader.iter(&events) {
-        let handle = asset_sever.load(dbg!(path).as_path());
-        commands.spawn(SpriteBundle {
-            material: materials.add(handle.into()),
-            ..Default::default()
-        });
-    }
 }
